6d2aa13fab0b7241edd715041e48f2343e6c3f04
FouadEAF <fouad.azbi@gmail.com>
2024-03-13 10:43:26 -0700

    Ambient Namespaces

--- 0x04-TypeScript/task_3/js/crud.js
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -1,14 +1,15 @@
 export function insertRow(row) {
-    console.log('Insert row', row);
-    return Math.floor(Math.random() * Math.floor(1000));
+  console.log('Insert row', row);
+  return Math.floor(Math.random() * Math.floor(1000));
 }
 
-export function deleteRow(idRow) {
-    console.log('Delete row id are ', idRow);
-    return;
+export function deleteRow(rowId) {
+  console.log('Delete row id', rowId);
+  return;
 }
 
-export function updateRow(idRow, row) {
-    console.log(`Update id row  is ${idRow}`, row);
-    return idRow;
+export function updateRow(rowId, row) {
+  console.log(`Update row ${rowId}`, row);
+
+  return rowId;
 }
