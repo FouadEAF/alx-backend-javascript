cbaa3e04fd5210139b1228e70e28d9abdeb31155
FouadEAF <fouad.azbi@gmail.com>
2024-04-30 13:45:26 -0700

    function named displayMessage that prints in STDOUT the string argument

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Fouad El Azbi
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
